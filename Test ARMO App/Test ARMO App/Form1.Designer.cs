9f557f7e8671b578509017fe517f0638e4d37aff
FriSha <stunstc@gmail.com>
2020-03-15 02:13:24 +0300

    Добавление проекта

---
+++ Test ARMO App/Test ARMO App/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Test_ARMO_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.treeView1 = new Test_ARMO_App.Form1.BufferedTreeView();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1072, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1006, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "*";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1053, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Путь к папке:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Шаблон имени файла:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Текст в файле:";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder.png");
+            this.imageList1.Images.SetKeyName(1, "file.png");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(380, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Начать поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1182, 132);
+            this.panel1.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(775, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(395, 30);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Отменить поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(389, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(380, 30);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Остановить поиск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSeparator1,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 728);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1182, 25);
+            this.toolStrip1.TabIndex = 11;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.AutoSize = false;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(650, 22);
+            this.toolStripLabel1.Text = "Поиск не идёт";
+            this.toolStripLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.AutoSize = false;
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(103, 22);
+            this.toolStripLabel2.Text = "Таймер стоит";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 131);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(1182, 597);
+            this.treeView1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Поиск файлов";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.Timer timer1;
+        private Test_ARMO_App.Form1.BufferedTreeView treeView1;
+    }
+}
+
---
+++ Test ARMO App/Test ARMO App/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Test_ARMO_App
+{
+    public partial class Form1 : Form
+    {
+        delegate void CurrentFileDelegate(string currentFile); //Делегат который выводит текущий обрабатываемый файл внизу экрана
+        CurrentFileDelegate CFD;
+
+        delegate void AddNodeDelegate(string currentDir, string currentFile, bool first); //Делегат добавляющий добавляет найденные файлы, а так же главную папку в TreeView
+        AddNodeDelegate AND;
+
+        delegate void SearchNodeDelegate(string currentDir, string currentPath); //Делегат позволяющий перейти к верхему узлу при возврате к верхней папке, а также добавляет папки в TreeView
+        SearchNodeDelegate SND;
+
+        delegate void EndSearchDelegate(); //Делегат заканчивающий поиск
+        EndSearchDelegate ESD;
+
+        Thread searchThread; //Объявление поискового потока
+
+        TreeNode currentNode; //Текущий используемый узел
+        string currentPath; //Текущий используемый путь
+
+        TimeSpan t = new TimeSpan(0); //Пройденное время
+
+        bool isSearchStopped = true; //Статус поиска
+
+        class BufferedTreeView : TreeView //Класс который позволяет TreeView использовать DoubleBuffer
+        {
+            protected override void OnHandleCreated(EventArgs e)
+            {
+                SendMessage(this.Handle, TVM_SETEXTENDEDSTYLE, (IntPtr)TVS_EX_DOUBLEBUFFER, (IntPtr)TVS_EX_DOUBLEBUFFER);
+                base.OnHandleCreated(e);
+            }
+            private const int TVM_SETEXTENDEDSTYLE = 0x1100 + 44;
+            private const int TVM_GETEXTENDEDSTYLE = 0x1100 + 45;
+            private const int TVS_EX_DOUBLEBUFFER = 0x0004;
+            [DllImport("user32.dll")]
+            private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            AND = new AddNodeDelegate(AddNode);
+            SND = new SearchNodeDelegate(SearchNode);
+            ESD = new EndSearchDelegate(EndSearch);
+            CFD = new CurrentFileDelegate(OutputCurrentFile);
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Нажатие кнопки "Начать/Продолжить поиск"
+        {
+            if (isSearchStopped) //Если поиск не идёт
+            {
+                string directory = textBox1.Text;
+                if (directory.Last().ToString() != "\\") //Если конце директории отсутствует "\", добавить в конец
+                {
+                    directory += "\\";
+                }
+
+                searchThread = new Thread(() => SearchThread(directory, textBox2.Text, textBox3.Text)); //Объявление поискового потока searchThread и перевод в него переменных пути, шаблона и текста которые нужны для поиска
+
+                treeView1.Nodes.Clear(); //Очистка TreeView если он был заполнен ранним поиском
+                t = new TimeSpan(0); //Обнуление таймера
+
+                timer1.Start(); //Старт таймера
+                toolStripLabel2.Text = "Прошло времени: 00:00:00";
+
+                button1.Enabled = false; //Отключение и включение нужных кнопок и полей для ввода
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+
+                isSearchStopped = false; //Перевод в режим поиска
+                button1.Text = "Продолжить поиск";
+
+                searchThread.Start(); //Начатие поискового потока
+            }
+            else //Если поиск был на паузе
+            {
+                searchThread.Resume(); //Продолжить поток
+                timer1.Start(); //Продолжить таймер
+
+                button1.Enabled = false; //Отключение и включение нужных кнопок
+                button2.Enabled = true;
+            }   
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Нажатие кнопки "Остановить поиск"
+        {
+            searchThread.Suspend(); //Приостановить поток
+            timer1.Stop(); //Приостановить таймер
+
+            button1.Enabled = true; //Отключение и включение нужных кнопок
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Нажатие кнопки "Отменить поиск"
+        {
+            if (searchThread.ThreadState == ThreadState.Suspended) //Если поток был на паузе, продолжить его
+                searchThread.Resume();
+            searchThread.Abort(); //Отменить поток
+
+            EndSearch(); //Действия связанные с остановкой поиска
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //Когда проходит 1 секунда на таймере
+        {
+            t = t.Add(new TimeSpan(10000000)); //Добавить секунду к прошедшему времени
+            toolStripLabel2.Text = "Прошло времени: " + t.ToString("hh\\:mm\\:ss"); //Обновить текст
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) //Когда закрываем окно
+        {
+            if (searchThread != null) //Если поток был создан
+            {
+                if (searchThread.ThreadState == ThreadState.Suspended) //Если поток был на паузе, продолжить его
+                    searchThread.Resume();
+                searchThread.Abort(); //Отменяем поток
+            }
+        }
+
+        //Процедуры связанные с потоком
+
+        public void SearchThread(string chosenDir, string nameTemplate, string searchText) //Поток в котором происходит процедура поиска
+        {
+            Invoke(AND, chosenDir, Path.GetFileName(chosenDir), true); //Добавление главной папки в которой происходит поиск как главный узел в TreeView
+
+            try
+            {
+                var foundFiles = Directory.EnumerateFiles(chosenDir, nameTemplate); //Нахождение всех файлов в главной папке которые соответствуют шаблону
+
+                foreach (string currentFile in foundFiles) //Для каждого найденного файла
+                {
+                    Invoke(CFD, currentFile); //Указать какой файл обрабатывается
+                    bool found = false;
+                    foreach (var line in File.ReadAllLines(currentFile)) //Для каждой строки в файле
+                    {
+                        if (line.Contains(searchText)) //Если найден текст указанный пользователем, указать что найдено и отменить поиск в файле
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) //Если текст был найден, добавить файл как узел к главному узлу
+                        Invoke(AND, currentFile, Path.GetFileName(currentFile), false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при поиске файлов: " + ex.Message);
+            }
+
+            RecursiveFolderSearch(chosenDir, nameTemplate, searchText); //Рекурсивный поиск файлов во всех вложенных папках
+            Invoke(ESD); //Закончить поиск
+        }
+
+        public void RecursiveFolderSearch(string currentDirectory, string nameTemplate, string searchText) //Рекурсивная процедура файлов во вложенных папках
+        {
+            try
+            {
+                var foundDirs = Directory.GetDirectories(currentDirectory);  //Нахождение всех папок в текущей директории
+                foreach (string currentDir in foundDirs) //Для каждой найденной папке
+                {
+                    currentPath = currentDir.Substring(0, currentDir.LastIndexOf("\\") + 1); //Указываем какую директорию мы сейчас используем (удалив всё после последнего "\")
+                    Invoke(SND, currentDir, currentPath); //Добавление найденной папки как узел к текущему узлу в TreeView
+
+                    try
+                    {
+                        var foundFiles = Directory.EnumerateFiles(currentDir, nameTemplate); //Нахождение всех файлов в текущей папке которые соответствуют шаблону
+
+                        foreach (string currentFile in foundFiles) //Для каждого найденного файла
+                        {
+                            Invoke(CFD, currentFile); //Указать какой файл обрабатывается
+                            bool found = false;
+                            foreach (var line in File.ReadAllLines(currentFile)) //Для каждой строки в файле
+                            {
+                                if (line.Contains(searchText)) //Если найден текст указанный пользователем, указать что найдено и отменить поиск в файле
+                                {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if (found) //Если текст был найден, добавить файл как узел к главному узлу
+                                Invoke(AND, currentFile, Path.GetFileName(currentFile), false);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Ошибка при поиске файлов: " + ex.Message);
+                    }
+
+                    RecursiveFolderSearch(currentDir, nameTemplate, searchText); //Захождение в найденную папку через рекурсию
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при отрытии директорий: " + ex.Message);
+            }
+        }
+
+        void OutputCurrentFile(string currentFile) //Вывод текущего обрабатываемого файла
+        {
+            toolStripLabel1.Text = "Файл: " + currentFile;
+        }
+
+        void AddNode(string currentDir, string currentFile, bool first) //Добавление файлов и главной папки как узлы в TreeView
+        {
+            if (first) //Если это главная папка
+                currentNode = treeView1.Nodes.Add(currentDir, Path.GetFileName(currentDir.TrimEnd('\\')), 0); //Добавляем и указываем текущий используемый узел
+            else //Если это файл
+                currentNode.Nodes.Add(currentDir, currentFile, 1); //Добавляем как узел к текущему узлу
+        }
+
+        void SearchNode(string currentDir, string currentPath) //Переход к верхему узлу при возврате к верхней папке и добавление папки в TreeView
+        {
+            currentNode = treeView1.Nodes.Find(currentPath, true)[0]; //Нахождение верхнего узла по текущей используемой папке
+            currentNode = currentNode.Nodes.Add(currentDir + '\\', Path.GetFileName(currentDir), 0); //Добавление папки как узел к текущему узлу в TreeView
+        }
+
+        void EndSearch() //Окончание поиска
+        {
+            button1.Enabled = true; //Отключение и включение нужных кнопок и полей для ввода
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+
+            isSearchStopped = true; //Выход из режима поиска
+            button1.Text = "Начать поиск";
+
+            toolStripLabel1.Text = "Поиск закончен";
+            toolStripLabel2.Text = "Заняло времени: " + t.ToString("hh\\:mm\\:ss");
+            timer1.Stop();
+        }
+    }
+}
